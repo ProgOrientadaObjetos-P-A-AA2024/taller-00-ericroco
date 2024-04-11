af7662e6bb764a06126af014066bbda2dfb43e81
ericroco <erickrodas55@gmail.com>
2024-04-11 16:51:35 -0500

    Taller00 (2)

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package paquete2;
+
 import paquete1.Mensaje;
 import paquete3.Informacion;
 
@@ -17,9 +18,12 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.printf("%s con %d\n",Mensaje.obtenerMensaje(),Informacion.obtenerHabitantes()    );
-     
+        /* Despúes de importar las clases llamamos las funciones que deseabamos 
+        imprimir para de esa manera darles uso en el printf en la frase
+        */
+        System.out.printf("%s con %d\n", Mensaje.obtenerMensaje(),
+                 Informacion.obtenerHabitantes());
+
     }
-    
+
 }
